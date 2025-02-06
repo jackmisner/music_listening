2722b988be525d4ea4cc28f410c0524226a0d852
Jack Misner <jack.misner@gmail.com>
2025-02-06 16:55:12 +0000

    got things kind of working

---
+++ lib/music_listening.py
@@ -0,0 +1,38 @@
+class Album():
+    def __init__(self, artist, album, song_list):
+        self.artist = artist
+        self.album = album
+        self.song_list = song_list
+
+    def __repr__(self):
+        return f"Artist: {self.artist}, Album: {self.album}, Songs: {', '.join(self.song_list)}"
+
+revolver = Album("The Beatles", "Revolver", ["Taxman","Eleanor Rigby","Here, There and Everywhere","Tomorrow Never Knows","Good Day Sunshine"])
+wtsmg = Album("Oasis", "What's The Story, Morning Glory", ["Wonderwall","Don't Look Back in Anger","Champagne Supernova","Roll with It","Some Might Say"])
+disintigration = Album("The Cure", "Disintigration", ['Lovesong','Pictures of You','Lullaby','Fascination Street','Prayers for Rain'])
+ok_computer = Album("Radiohead", "OK Computer", ["Paranoid Android","No Surprises","Karma Police","Exit Music (For a Film)","Lucky"])
+wpsiatwin = Album('Arctic Monkeys', "Whatever People Say I Am: That's What I'm Not", ["I Bet You Look Good on the Dancefloor","Fake Tales of San Francisco","When the Sun Goes Down","The View from the Afternoon","A Certain Romance"])
+
+class Music_Listener():
+    def __init__(self, name):
+        self.name = name
+        self.listened_to_dict = available_songs_dict = {}
+        self.available_music_list = [disintigration, wpsiatwin, revolver,wtsmg, ok_computer]
+   
+    def listen_to(self, listened_to_song):
+        self.listened_to_song = listened_to_song
+        for item in self.available_music_list:
+            print(item)
+            for song in item.song_list:
+                if song == listened_to_song:
+                    self.listened_to_dict[item.artist] = {item.album:listened_to_song}
+        print(self.listened_to_dict)
+
+
+
+
+# class Song():
+#             # name, album, artist 
+
+
+#             [{"Taxman": {artist: , album_title: }]
--- tests/test_music_listening.py
+++ tests/test_music_listening.py
@@ -1,2 +1,40 @@
 import pytest
 from lib.music_listening import *
+
+ml = Music_Listener("Jackseph")
+# Is there a music listener?
+
+def test_is_there_a_listener():
+    ml = Music_Listener("Jackseph")
+
+# Check for list of available songs
+
+def test_are_there_songs_to_listen_to():
+    assert len(ml.available_music_list) > 0
+
+# Check that listened to songs starts out empty
+
+def test_listened_to_dict_starts_empty():
+    assert len(ml.listened_to_dict) == 0 
+
+def test_listen_to_track_lovesong():
+    ml.listen_to("Lovesong")
+    assert "The Cure" in ml.listened_to_dict.keys()
+    assert "Disintigration" in ml.listened_to_dict["The Cure"].keys()
+    assert "Lovesong" in ml.listened_to_dict["The Cure"]["Disintigration"]
+
+def test_listen_to_track_taxman():
+    ml.listen_to("Taxman")
+    assert "The Beatles" in ml.listened_to_dict.keys()
+    assert "Revolver" in ml.listened_to_dict["The Beatles"].keys()
+    assert "Taxman" in ml.listened_to_dict["The Beatles"]["Revolver"]
+
+def test_return_listened_to():
+    ml.listened_to_dict = {}
+    ml.listen_to('Taxman') 
+    assert ml.listened_to_dict == {"The Beatles": {"Revolver": ["Taxman"]}}
+    ml.listen_to('No Surprises')
+    assert ml.listened_to_dict == {"The Beatles": {"Revolver": ["Taxman"]}, "Radiohead": {"OK Computer": ["No Surprises"]}}
+    ml.listen_to('Lucky')
+    assert ml.listened_to_dict == {"The Beatles": {"Revolver": ["Taxman"]}, "Radiohead": {"OK Computer": ["No Surprises", "Lucky"]}}
+
